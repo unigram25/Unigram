19df268f9edcddd4b9bd9d312d73cf71fd8e4462
siva s <sivaastronaut@gmail.com>
2025-02-08 08:18:50 +0000

    Configure test 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const App = () => {
+function App() {
   return (
-    <div><h1 className="text-3xl font-bold underrline"> Hello World</h1></div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <p className="text-lg text-gray-700 mt-2">
+        This is a sample React + Vite + Tailwind CSS setup.
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
