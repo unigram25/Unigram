e7f2a237f86f3a662f6c0b2625f655185f8c2a71
siva s <sivaastronaut@gmail.com>
2025-02-18 17:03:30 +0000

    just update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './components/Navbar'
+import { Login } from '@mui/icons-material'
+import Login_main from './pages/Login_main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
+      <Login_main/>
     </>
   )
 }
---
+++ frontend/src/pages/Login_main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login_main = () => {
+  return (
+    <div>
+
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Login_main
